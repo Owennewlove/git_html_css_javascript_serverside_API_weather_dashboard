a3f0908d3a3ca95024880ffd4f3226c94a301ef6
Owen Newlove <owen.newlove@gmail.com>
2022-06-10 22:23:10 -0400

    added console logs to try to figure out how to utilize the data and have given up for the night

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var APIKey = "5865efb9befd52ec6fc44fd061b582bf";
 
 var cities = [];
 
+var topSectionCity = document.getElementById("city")
 
 function getVal() {
 
@@ -21,39 +22,49 @@ var searchInput = document.querySelector("input").value;
 
 var searchBtn = document.getElementById("searchButton")
 
-searchBtn.addEventListener("click", function (){
+searchBtn.addEventListener("click", function () {
 
 
     getVal();
     var city = getVal();
     console.log(city)
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial"
 
-    fetch(queryURL) 
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        cities.push(data)
-        console.log(cities)
-      });
+    fetch(queryURL)
+        .then(function (response) {
 
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
 
-     
+            cities.push(data)
+            localStorage.setItem("city", JSON.stringify(cities))
+        });
 
-
-   
-
-   
-
-   
+    console.log("clicked")
+    console.log(cities.values(name));
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
