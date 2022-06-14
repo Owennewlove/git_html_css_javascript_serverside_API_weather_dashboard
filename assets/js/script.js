e15b67842cf0212f34ce022b3036c981f6a265f3
Owen Newlove <owen.newlove@gmail.com>
2022-06-13 20:53:43 -0400

    created variables that referenced the first and second days of the five day forecast. Then called upon the temp, the icon, the date, the wind speed, and the humidity for the respective day and then added these called upon items into their respective places

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,28 @@ var uviEl = document.getElementById("uvIndex")
 
 var windEl = document.getElementById("wind")
 
+var TempEl2 = document.getElementById("temp2")
+
+var humidityEl2 = document.getElementById("humidity2")
+
+var windEl2 = document.getElementById("wind2")
+
+var firstDay = document.getElementById("dayOne")
+
+var TempEl3 = document.getElementById("temp3")
+
+var humidityEl3 = document.getElementById("humidity3")
+
+var windEl3 = document.getElementById("wind3")
+
+var secondDay = document.getElementById("dayTwo")
+
+
+
+
+
+
+
 function getVal() {
 
     const val = document.querySelector("input").value;
@@ -66,19 +88,56 @@ searchBtn.addEventListener("click", function () {
                 var currentDate= moment.unix(data.dt).format("MM/DD/YYYY")
                 var iconImage=document.createElement("img")
                 iconImage.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-                topSectionCity.innerHTML=  data.name + " "+ currentDate
+                topSectionCity.innerHTML=  data.name + " ("+ currentDate + ")"
                 topSectionCity.appendChild(iconImage)
 
                 
-                TempEl.textContent= "Temperature: " + data.main.temp + "°F"
+                TempEl.textContent= "Temp: " + data.main.temp + "°F"
 
-                windEl.textContent = "Wind: " + fiveData.daily[0].wind_speed + " mph"
+                windEl.textContent = "Wind: " + fiveData.daily[0].wind_speed + " MPH"
 
-                uviEl.textContent = "UVI: " + fiveData.daily[0].uvi
+                uviEl.textContent = "UV Index: " + fiveData.daily[0].uvi
 
                 humidityEl.textContent = "Humidity: " + data.main.humidity + "%"
 
                 localStorage.setItem("city", JSON.stringify(cities))
+
+                var Day1Date = moment.unix(fiveData.daily[1].dt).format("MM/DD/YYYY")
+
+                firstDay.innerHTML = Day1Date
+
+                var iconImage1=document.createElement("img")
+
+                iconImage1.setAttribute("src",`http://openweathermap.org/img/wn/${fiveData.daily[1].weather[0].icon}@2x.png`)
+                
+                iconOne.appendChild(iconImage1)
+
+                
+
+                TempEl2.textContent= "Temp: " + fiveData.daily[1].temp.day + "°F"
+
+                windEl2.textContent = "Wind: " + fiveData.daily[1].wind_speed + " MPH"
+
+                humidityEl2.textContent = "Humidity: " + fiveData.daily[1].humidity + "%"
+
+
+                var Day2Date = moment.unix(fiveData.daily[2].dt).format("MM/DD/YYYY")
+
+                secondDay.innerHTML = Day2Date
+
+                var iconImage2=document.createElement("img")
+
+                iconImage2.setAttribute("src",`http://openweathermap.org/img/wn/${fiveData.daily[1].weather[0].icon}@2x.png`)
+                
+                iconTwo.appendChild(iconImage2)
+
+                
+
+                TempEl3.textContent= "Temp: " + fiveData.daily[2].temp.day + "°F"
+
+                windEl3.textContent = "Wind: " + fiveData.daily[2].wind_speed + " MPH"
+
+                humidityEl3.textContent = "Humidity: " + fiveData.daily[2].humidity + "%"
                  
 
             })
