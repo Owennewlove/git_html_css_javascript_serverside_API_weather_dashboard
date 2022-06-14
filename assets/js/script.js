e0c0b99809278f8bfa8d0b1fdf3ba85183c3ae27
Owen Newlove <owen.newlove@gmail.com>
2022-06-13 20:13:17 -0400

    created variables that referenced the top section temperature, wind, humidity, and uv Index. Then added another fetch and then function that adds the api elements that were called to the appropriate variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,30 @@ var cities = [];
 
 var topSectionCity = document.getElementById("city")
 
+
+var TempEl = document.getElementById("temp")
+
+var humidityEl = document.getElementById("humidity")
+
+var uviEl = document.getElementById("uvIndex")
+
+var windEl = document.getElementById("wind")
+
 function getVal() {
 
     const val = document.querySelector("input").value;
     return val;
 }
 
+function displaCitySearchList() {
 
+    if (localStorage.getItem("city")) {
+        cities = JSON.parse(localStorage.getItem("city"))
+    }
+    console.log(cities)
+
+
+}
 
 
 var searchInput = document.querySelector("input").value;
@@ -27,7 +44,7 @@ searchBtn.addEventListener("click", function () {
 
     getVal();
     var city = getVal();
-    console.log(city)
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial"
 
     fetch(queryURL)
@@ -36,14 +53,42 @@ searchBtn.addEventListener("click", function () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
 
-            cities.push(data)
-            localStorage.setItem("city", JSON.stringify(cities))
+
+            var fiveDayUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${APIKey}&units=imperial
+            `
+            fetch(fiveDayUrl)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(fiveData){
+                console.log(fiveData)
+                var currentDate= moment.unix(data.dt).format("MM/DD/YYYY")
+                var iconImage=document.createElement("img")
+                iconImage.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                topSectionCity.innerHTML=  data.name + " "+ currentDate
+                topSectionCity.appendChild(iconImage)
+
+                
+                TempEl.textContent= "Temperature: " + data.main.temp + "°F"
+
+                windEl.textContent = "Wind: " + fiveData.daily[0].wind_speed + " mph"
+
+                uviEl.textContent = "UVI: " + fiveData.daily[0].uvi
+
+                humidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+
+                localStorage.setItem("city", JSON.stringify(cities))
+                 
+
+            })
+
+
+
         });
 
     console.log("clicked")
-    console.log(cities.values(name));
+
 
 
 
@@ -69,3 +114,4 @@ searchBtn.addEventListener("click", function () {
 
 })
 
+displaCitySearchList();
